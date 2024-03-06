aaf9bf98cd7af6c6274d341e348ba4ebdb9cb60a
Mohini <upretimohini@gmail.com>
2024-03-06 16:46:57 +0530

    edti finalised

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -3,13 +3,13 @@ import { useState } from 'react';
 function Note(props) {
   const[IsEditing , setIsEditing]= useState(false);
   const [updatedContent , setUpdatedContent] = useState(props.content);
-
+  const [UpdateTitle , setUpdateTitle] = useState(props.title);
   function handleClick() {
     props.onDelete(props.id);
   }
 
   function handleEdit(){
-    setIsEditing(true);
+    setIsEditing(!IsEditing);
   }
   
   function handleSave(){
@@ -18,17 +18,28 @@ function Note(props) {
 
   return (
     <div className="note">
-      <h1>{props.title}</h1>
+      <h1>
+      {IsEditing ? (<input type = "text" value={UpdateTitle} onChange = {
+        (e)=>{
+          setUpdateTitle(e.target.value);
+
+        }
+           }
+        />) :<h1> {UpdateTitle} </h1>}
+       
+      </h1>
+      <p>
+
       {IsEditing ? (
         <input type="text" value={updatedContent} onChange=
         {  
           (e) => 
              setUpdatedContent(e.target.value)       
-             } onBlur={handleSave} />
+             } />
       )  : (
-        <p onClick={handleEdit}>{updatedContent}</p> // Allow editing when the paragraph is clicked
+        <p >{updatedContent}</p> 
       )}
-      <p></p>
+      </p>
       <button onClick={handleClick}>DELETE</button>
       <button onClick = {handleEdit}> {(IsEditing) ? "SAVE" : "EDIT"}</button>
     </div>
